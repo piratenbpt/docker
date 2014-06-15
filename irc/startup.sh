f2a2bafbbaa51d698af7325bab87540c6ef8308e
Patrick Rauscher <prauscher@ohai.su>
2014-06-15 20:49:19 +0200

    use more parameters in irc

--- irc/startup.sh
+++ irc/startup.sh
@@ -7,41 +7,56 @@ function replaceParameter() {
 }
 
 cat /etc/charybdis/ircd.conf.sample \
+	| replaceParameter "SERVERNAME" "$SERVERNAME" \
+	| replaceParameter "SERVERDESC" "$SERVERDESC" \
+	| replaceParameter "SERVERID" "$SERVERID" \
+	| replaceParameter "NETWORKNAME" "$NETWORKNAME" \
+	| replaceParameter "NETWORKDESC" "$NETWORKDESC" \
+	| replaceParameter "ADMINNAME" "$ADMINNAME" \
+	| replaceParameter "ADMINMAIL" "$ADMINMAIL" \
+	| replaceParameter "ADMINDESC" "$ADMINDESC" \
 	| replaceParameter "PASSWORD-CONNECT" "$PASSWORD_CONNECT" \
 	| replaceParameter "PASSWORD-WEBCHAT" "$PASSWORD_WEBCHAT" \
 	| replaceParameter "PASSWORD-OPER" "$PASSWORD_OPER" \
 	> /etc/charybdis/ircd.conf
 
-# Add remote
-[ "$REMOTE_TYPE" = "stunnel" ] && {
-	stunnel4 -fd 0 <<EOT
+for i in $(env | grep "^REMOTE_" | awk -F_ '{ print $1"_"$2 }' | sort | uniq); do
+	v="${i}_NAME";			name=${!v}
+	v="${i}_TYPE";			type=${!v}
+	v="${i}_HOST";			host=${!v}
+	v="${i}_PORT";			port=${!v}
+	v="${i}_PASSWORD_SEND";		password_send=${!v}
+	v="${i}_PASSWORD_ACCEPT";	password_accept=${!v}
+
+	if [ "$type" = "stunnel" ]; then
+		stunnel4 -fd 0 <<EOT
 [ircd]
 client = yes
 accept = 127.0.0.1:10001
-connect = $REMOTE_HOST:$REMOTE_PORT
+connect = $host:$port
 EOT
-	REMOTE_TYPE="raw"
-	REMOTE_HOST="127.0.0.1"
-	REMOTE_PORT="10001"
+		type="raw"
+		host="127.0.0.1"
+		port="10001"
 
-	# stunnel quite sucks.
-	nc $REMOTE_HOST $REMOTE_PORT <<EOT
-EOT
-}
+		# stunnel quite sucks.
+		echo | nc $host $port
+	fi
 
-[ "$REMOTE_TYPE" = "raw" ] && {
-	cat >> /etc/charybdis/ircd.conf <<EOT
-connect "vpn01.piratenpartei-hessen.de" {
-	host = "$REMOTE_HOST";
-	port = "$REMOTE_PASS";
-	send_password = "$REMOTE_PASSWORD_SEND";
-	accept_password = "$REMOTE_PASSWORD_ACCEPT";
+	if [ "$type" = "raw" ]; then
+		cat >> /etc/charybdis/ircd.conf <<EOT
+connect "$name" {
+	host = "$host";
+	port = "$port";
+	send_password = "$password_send";
+	accept_password = "$password_accept";
 	hub_mask = "*";
 	class = "server";
 	flags = autoconn;
 };
 EOT
-}
+	fi
+done
 
 # start ircd
 sudo -u charybdis /usr/bin/charybdis-ircd -foreground
